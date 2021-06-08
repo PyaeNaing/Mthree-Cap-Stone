1d74104179738ac53e0c8c976e601ee709e089d0
Samuel Thomas <54728781+Wolfcastle1@users.noreply.github.com>
2021-06-08 10:52:34 -0500

    HomePage created App.js updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
-import React, {useState} from 'react'
-
-  return (
-    <div>
-        <h1> HELLO WORLD! </h1>
-    </div>
-  )
-
+import React, { Component } from "react";
+import HomePage from "./pages/HomePage";
+ 
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <HomePage/>
+      </div>
+    );
+  }
+}
+ 
 export default App;
