ab3df56c7bdb05c4040ee6359c3047f6b9026921
Samuel Thomas <54728781+Wolfcastle1@users.noreply.github.com>
2021-06-10 13:20:48 -0500

    About Page added Contributors

--- client/src/pages/AboutPage.js
+++ client/src/pages/AboutPage.js
@@ -4,8 +4,8 @@ class AboutPage extends Component {
 
     render() {
         return (
-            <div className="container" style={{backgroundColor: "#6c6c82", padding: "10px", height: "100vh"}}>
-                <div className="text-center" style={{ backgroundColor: "#ffffff" }}>
+            <div className="container">
+                <div className="text-center" style={{ backgroundColor: "#cccccc", marginTop: "10vh"}}>
                     <h1>About Page</h1>
 
                     <p>
@@ -23,6 +23,16 @@ class AboutPage extends Component {
                     <p>
                         This allowed us to create an easy-to-use interface to show you flights and their dates to make your next vacation or business flight easier to plan!
                 </p>
+                </div>
+                
+                <div className="text-center" style={{ backgroundColor: "#cccccc", marginTop: "10vh"}}>
+                    <h1>Contributors</h1>
+                    <ul>
+                        <li>Pyae Naing</li>
+                        <li>Samuel Thomas</li>
+                        <li>Faisal AlGhamdi</li>
+                        <li>Sam Sagawa</li>
+                    </ul>
                 </div>
             </div>
         )
