7403864d33ce596471c0e00479c868d684a6b4ad
pyaenaing <pnaing@mail.sfsu.edu>
2021-06-11 08:08:10 -0700

    pushing what i have

--- Flights/src/main/java/com/mthree/capstone/controllers/FlightController.java
+++ Flights/src/main/java/com/mthree/capstone/controllers/FlightController.java
@@ -8,14 +8,12 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.UnsupportedEncodingException;
 import java.util.*;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/api/flight")
 public class FlightController {
